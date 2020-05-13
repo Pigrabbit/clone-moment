41d756da8535b6b227838a45b91ce295973f1173
Pigrabbit <donghyuk1003@gmail.com>
2020-05-13 14:01:18 +0900

    Add todo feature
    Get input from HTML and save it to local stoarge.
    Paint the contents stored in local storage back into HTML.

---
+++ todo.js
@@ -0,0 +1,54 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "❌";
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    span.innerText = text;
+    li.appendChild(deleteBtn);
+    li.appendChild(span);
+    toDoList.appendChild(li);
+    li.id = newId;
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit() {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parseToDos = JSON.parse(loadedToDos);
+        parseToDos.forEach(toDo => {
+            paintToDo(toDo.text);
+        });
+    }
+}
+
+function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit)
+}
+
+init();
